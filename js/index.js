625caf55c8172ccadab86de4b0549c22118512ff
Fustile <fustile99@gmail.com>
2019-11-05 08:35:15 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+    var players = {
+        one: {
+            state: false,
+            name: null
+        },
+        two: {
+            state: false,
+            name: null
+        },
+    }
+
+    $("#grid1").click(function(){
+        $("#grid1").css("background-color","red")
+    })
+
+    $("#changePlayer").click(function(){
+
+    })
+
+    $("#newPlayer").click(function(){
+        console.log(players.one)
+
+        if (players.one.state == false && $("#inputName").val() !== ""){
+            console.log("one")
+            players.one.name = $("#inputName").val()
+            players.one.state = true
+            console.log(players.one)
+        }
+        else if (players.two.state == false && $("#inputName").val() !== ""){
+            players.two.name = $("#inputName").val()
+            players.two.state = true
+            console.log(players.two)
+        }
+        console.log(players)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
