4205e3e0701fe5fb1b063e298a3b32e21394ef04
Fustile <fustile99@gmail.com>
2019-11-07 07:11:45 +0300

    homepage - finished playing page - started

--- js/index.js
+++ js/index.js
@@ -1,40 +1,69 @@
 $(document).ready(function(){
 
-    var players = {
-        one: {
-            state: false,
-            name: null
-        },
-        two: {
-            state: false,
-            name: null
-        },
-    }
-
-    $("#grid1").click(function(){
-        $("#grid1").css("background-color","red")
-    })
-
-    $("#changePlayer").click(function(){
-
-    })
-
-    $("#newPlayer").click(function(){
-        console.log(players.one)
-
-        if (players.one.state == false && $("#inputName").val() !== ""){
-            console.log("one")
-            players.one.name = $("#inputName").val()
-            players.one.state = true
-            console.log(players.one)
+    $("#usernameSubmit").click(function(){
+        if ($("#usernameInput").val() == ""){
+            $("#usernameInput").css('outline', 'solid 1px red');
+            return
         }
-        else if (players.two.state == false && $("#inputName").val() !== ""){
-            players.two.name = $("#inputName").val()
-            players.two.state = true
-            console.log(players.two)
-        }
-        console.log(players)
+        console.log("submit clicked")
+        $("#usernameInput").hide()
+        $("#usernameSubmit").hide()
+        $(".username").text($("#usernameInput").val())
     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // var players = {
+    //     one: {
+    //         state: false,
+    //         name: null
+    //     },
+    //     two: {
+    //         state: false,
+    //         name: null
+    //     },
+    // }
+
+    // $("#grid1").click(function(){
+    //     $("#grid1").css("background-color","red")
+    // })
+
+    // $("#changePlayer").click(function(){
+
+    // })
+
+    // $("#newPlayer").click(function(){
+    //     console.log(players.one)
+
+    //     if (players.one.state == false && $("#inputName").val() !== ""){
+    //         console.log("one")
+    //         players.one.name = $("#inputName").val()
+    //         players.one.state = true
+    //         console.log(players.one)
+    //     }
+    //     else if (players.two.state == false && $("#inputName").val() !== ""){
+    //         players.two.name = $("#inputName").val()
+    //         players.two.state = true
+    //         console.log(players.two)
+    //     }
+    //     console.log(players)
+    // })
 
 
 
