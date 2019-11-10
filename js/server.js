91d033f0e173997c841326b79c642774a3d63e5d
Fustile <fustile99@gmail.com>
2019-11-10 15:05:24 +0300

    small adjustments

--- js/server.js
+++ js/server.js
@@ -8,73 +8,81 @@ var db
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-var players = [
+var games = [
     {
         id: 1,
-        name: "Player One"
+        name: "Game One"
     },
     {
         id: 2,
-        name: "Player Two"
+        name: "Game Two"
     },
     {
         id: 3,
-        name: "Player Three"
+        name: "Game Three"
     }
 ]
 
-var games = [
-    {
-
-    }
-]
 
 
 app.get("/hello", function(req, res){
-    res.send("Hello API")
+    res.send(db)
 })
 
-app.get("/players/list", function(req, res){
-    res.send(players)
-})
-
-app.get("/players/:id", function(req, res){
-    console.log(req.params)
-    var player = players.find(function (player) {
-        return player.id === Number(req.params.id)
-    })
-    res.send(player.name)
-})
-
-app.post("/playersadd", function (req, res) {
-    var player = {
-        id: Date.now(),
-        name: req.body.name
-    }
-    players.push(player)
-    console.log(req.body)
-    res.send(player)
-})
-
-app.put("/players/:id", function (req,res) {
-    var player = players.find(function (player) {
-        return player.id === Number(req.params.id)
-    })
-    player.name = req.body.name
-    res.sendStatus(200)
-})
-
-app.delete("/players/:id", function (req, res){
-    players = players.filter(function (player){
-        return player.id !==Number(req.params.id)
-    })
-    res.sendStatus(200)
-})
-
-app.get("/players/list", function(req, res){
+app.get("/games/list", function(req, res){
     res.send(games)
 })
 
-app.listen(4000, function() {
-    console.log("API app started on port 4000")
+app.get("/games/:id", function(req, res){
+    console.log(req.params)
+    var game = games.find(function (game) {
+        return game.id === Number(req.params.id)
+    })
+    res.send(game.name)
+})
+
+app.post("/games/new", function (req, res, db) {
+    var game = {
+        name: req.body.name
+    }
+
+    // db.createCollection(games)
+    db.collection("games").insert(game, function(err, result){
+        if (err) {
+            console.log(err)
+            res.sendStatus(500)
+        }
+        res.send(game)
+    })
+})
+
+app.put("/games/:id", function (req,res) {
+    var game = games.find(function (game) {
+        return game.id === Number(req.params.id)
+    })
+    game.name = req.body.name
+    res.sendStatus(200)
+})
+
+app.delete("/games/:id", function (req, res){
+    games = games.filter(function (game){
+        return game.id !==Number(req.params.id)
+    })
+    res.sendStatus(200)
+})
+
+app.get("/games/list", function(req, res){
+    res.send(games)
+})
+
+
+
+MongoClient.connect('mongodb://localhost:27017/tictactoe', function(err, database){
+    if (err) {
+        return console.log(err)
+    }
+    db = database
+    app.listen(4000, function() {
+        console.log("API app started on port")
+    })
 })
